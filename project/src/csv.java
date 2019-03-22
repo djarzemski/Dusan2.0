f498fb846d80a5c755c1fdc14b6e21c0af4e32be
djarzemski <44023523+djarzemski@users.noreply.github.com>
2019-03-22 09:29:40 +0100

    Update csv.java

--- project/src/csv.java
+++ project/src/csv.java
@@ -7,24 +7,38 @@ public class csv {
 
     public static void main(String[] args){
         public csv(String csv_path) throws FileNotFoundException{
+            public ArrayList<Student> students = new ArrayList<>();
+            
             Scanner scanner = new Scanner(new File(csv_path));
             scanner.useDelimiter(",|\r\n");
+            
+            String mail = "";
+            String hall = "";
+            String name = "";
+            String year = "";
 
             int x = 0;
             while (scanner.hasNext()){
                 switch (x++){
                     case 0:
-
+                        mail = scanner.next();
                         break;
 
                     case 1:
-
+                        hall = scanner.next();
                         break;
                     case 2:
-
+                        name = scanner.next();
                         break;
                     case 3:
-
+                        year = scanner.next();
+                        
+                        students.add(new Student(mail, hall, name, year);
+                                     
+                        mail = "";
+                        hall = "";
+                        name = "";
+                        year = "";             
                         x = 0;
                         break;
                 }
