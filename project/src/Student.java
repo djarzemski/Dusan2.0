959420a3e65edbc55327269ed09059994a21ecca
Filip Mihal <fifomihal@gmail.com>
2019-03-24 15:58:35 +0100

    Refactor of Student class

--- project/src/Student.java
+++ project/src/Student.java
@@ -1,61 +1,89 @@
+enum Gender{
+    MALE, FEMALE
+}
+
 public class Student {
-  
-  private String mail;
-  
-  /*
-      0 - founders;
-      1 - gentlemen;
-      2 - fortes;
-      3 - sprouts;
-  */
-  private int hall;
-  
-  
-  /*
-      0 - male;
-      1 - female;
-  */
-  private int gender;
-  
-  private String name;
-  
-  private int year;
-  
-  public Student(String mail, String name, String hall, String year){
-    setMail(mail);
-    setName(name);
-    setHall(hall);
-    setYear(year);
-  }
-  
-  public String setMail(String mail){
-    this.mail = mail;
-  }
-  
-  public String setName(String name){
-    this.name = name;
-  }
-  
-  public int setHall(String hall){
-    if(hall=="Founders") this.hall = 0;
-    if(hall=="Gentlemen") this.hall = 1;
-    if(hall=="Fortes") this.hall = 2;
-    if(hall=="Sprouts") this.hall = 3;
-    
-    setGender(this.hall);
-  }
-  
-  public setGender(int hall){
-    if(hall == 0 || hall == 1) this.gender = 0;
-    if(hall == 2 || hall == 3) this.gender = 1;
-  }
-  
-  public setYear(year){
-   if(year == "YEAR 1") this.year = 1;
-   if(year == "YEAR 2") this.year = 2;
-   if(year == "YEAR 3") this.year = 3;
-   if(year == "YEAR 4") this.year = 4;
-   } 
- 
-  
+    private String email;
+    private String hall;
+    private String fullName;
+    private int year;
+    private Gender gender;
+
+    public Student(String email, String name, String hall, int year) {
+        this.email = email;
+        this.fullName = name;
+        this.hall = hall;
+        this.year = year;
+        this.gender = decodeGender(hall);
+    }
+
+    private static Gender decodeGender(String hall){
+        if(hall.equals("Founders") || hall.equals("Gentlemen")){
+            return Gender.MALE;
+        }
+        else{
+            return Gender.FEMALE;
+        }
+    }
+
+    private static int decodeYear(String year){
+        int newYear;
+        switch (year){
+            case "YEAR 1":
+                newYear = 1;
+                break;
+            case "YEAR 2":
+                newYear = 2;
+            case "YEAR 3":
+                newYear = 3;
+                break;
+            case "YEAR 4":
+                newYear = 4;
+                break;
+            default:
+                newYear = 0;
+                break;
+        }
+        return newYear;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getHall() {
+        return hall;
+    }
+
+    public void setHall(String hall) {
+        this.hall = hall;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setGender(Gender gender){
+        this.gender = gender;
+    }
+
+    public Gender getGender() {
+        return gender; 
+    }
 }
