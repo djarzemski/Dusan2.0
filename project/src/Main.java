c0c28ca95d0ecc739cd14ae5c76eb048c5be523a
Filip Mihal <fifomihal@gmail.com>
2019-03-24 16:36:33 +0100

    created a main class

---
+++ project/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        try {
+            students = Data.load("project/src/database.csv");
+        }catch (FileNotFoundException e){
+            System.out.println(e + "There is a problem with loading the data. Check if the csv file is in correct folder.");
+        }
+
+    }
+}
