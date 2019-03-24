0a37788e5399f17d296b08d70ef27b035c8e4de2
Filip Mihal <fifomihal@gmail.com>
2019-03-24 16:34:44 +0100

    data loader refactored

---
+++ project/src/Data.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Data {
+
+    public static ArrayList<Student> load(String path) throws FileNotFoundException{
+        ArrayList<Student> students = new ArrayList<>();
+
+        Scanner scanner = new Scanner(new File(path));
+        scanner.useDelimiter(",|\r\n");
+
+        String mail = "";
+        String hall = "";
+        String name = "";
+        String year = "";
+
+        int x = 0;
+        while (scanner.hasNext()){
+            switch (x++){
+                case 0:
+                    mail = scanner.next();
+                    break;
+
+                case 1:
+                    hall = scanner.next();
+                    break;
+                case 2:
+                    name = scanner.next();
+                    break;
+                case 3:
+                    year = scanner.next();
+
+                    students.add(new Student(mail, name, hall, year));
+
+                    mail = "";
+                    hall = "";
+                    name = "";
+                    year = "";
+                    x = 0;
+                    break;
+            }
+        }
+
+        scanner.close();
+        return students;
+    }
+}
