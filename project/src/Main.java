bdf391a0958346c1fd42d41e2b64c175859d54fa
Filip Mihal <fifomihal@gmail.com>
2019-03-25 10:52:31 +0100

    Revert "test"
    This reverts commit 56dbcff09e99f8e92e7a1e471bc1ee880185e2c9.

--- project/src/Main.java
+++ project/src/Main.java
@@ -5,18 +5,13 @@ public class Main {
 
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
-        //System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try {
-            students = Data.load("database.csv");
+            students = Data.load("project/src/database.csv");
         }catch (FileNotFoundException e){
             System.out.println(e + "There is a problem with loading the data. Check if the csv file is in correct folder.");
         }
 
-<<<<<<< HEAD
         System.out.println(students.get(0).getFullName());
 
-=======
-        System.out.print(students);
->>>>>>> 0b3c1e3435174b6ac54062da379cdfb281e4788d
     }
 }
