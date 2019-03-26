e3ac4e9f406d2beaf71f4f6124ed6d5aa07f4b73
Natália Slosiarová <natalia.slosiarova@student.leaf.academy>
2019-03-26 12:27:06 +0100

    methods grouping students

--- project/src/Groups.java
+++ project/src/Groups.java
@@ -1,2 +1,18 @@
+import java.util.*;
 public class Groups {
+    private ArrayList<ArrayList<Student>> groupsOfStudents = new ArrayList<ArrayList<Student>>();
+
+    public Groups(ArrayList<Student> students, int num){
+        shuffleList(students);
+    }
+    private static void shuffleList(ArrayList<Student> students){
+        Collections.shuffle(students);
+    
+    }
+    private static ArrayList<ArrayList<Student>> makeSubGroups(ArrayList<Student> students, int num){
+        for (Student student : students) {
+            
+            
+        }
+    }
 }
